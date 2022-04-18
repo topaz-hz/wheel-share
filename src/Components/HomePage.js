c05338b9a65be36058c10505f10a0aa1f3ea55d4
topaz-hz <topazhersh@gmail.com>
2022-04-18 20:53:53 +0300

    fix lint config

---
+++ src/Components/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to WheelShare</h1>
+      </div>
+    );
+  }
+}
