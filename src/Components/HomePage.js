f12f7c1bce030558123dd85372bde15b49a6e744
topaz-hz <topazhersh@gmail.com>
2022-04-19 11:48:10 +0300

    use grid on homepage for basic layout

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,30 +3,66 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
 import NavigationForm from './MapAndNavigation/NavigationForm';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     '& > *': {
-//       margin: theme.spacing(1)
-//     }
-//   }
-// }));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  container: {
+    height: '100vh',
+    width: '100vw',
+    textAlign: 'center',
+    fontFamily: 'Trebuchet MS, Helvetica, sans-serif'
+  },
+  grid: {
+    margin: '10px'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+  footer: {
+    position: 'fixed',
+    bottom: '0',
+    left: 0,
+    height: '50px',
+    width: '100%',
+    backgroundColor: 'lightblue'
+  }
+}));
 
 export default function HomePage() {
-  // const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <>
       <CssBaseline />
-      <Container
-        style={{
-          height: '100vh',
-          width: '100vw',
-          textAlign: 'center'
-        }}>
-        <h1 style={{ color: 'darkblue' }}>WheelShare</h1>
-        <Divider variant="middle" />
-        <NavigationForm />
+      <Container className={classes.container}>
+        <Grid className={classes.grid} container spacing={3}>
+          <Grid item xs={12}>
+            <h1 style={{ color: 'darkblue', fontSize: '42px' }}>WheelShare</h1>
+            <Divider variant="fullWidth" />
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>Put hazards list component here</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>Put hazard form component here</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <NavigationForm />
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>Put map component here</Paper>
+          </Grid>
+        </Grid>
+        <div className={classes.footer}>Put footer component here</div>
       </Container>
     </>
   );
