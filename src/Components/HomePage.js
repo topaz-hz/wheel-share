a58b205b480295eb1385cb666ca8b750eaea3005
topaz-hz <topazhersh@gmail.com>
2022-04-20 22:53:26 +0300

    delete api key & Find My Location (wip)

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
@@ -9,8 +9,9 @@ import NavigationForm from './MapAndNavigation/NavigationForm';
 import MapWrapper from './MapAndNavigation/MapWrapper';
 
 const markersList = [
-  { id: 1, position: { lat: -33.89, lng: 151.274 } },
-  { id: 2, position: { lat: -33.8, lng: 151.274 } }
+  { id: 1, position: { lat: 32.07, lng: 34.777 } },
+  { id: 2, position: { lat: 32.08, lng: 34.775 } },
+  { id: 3, position: { lat: 32.075, lng: 34.774 } }
 ];
 
 const useStyles = makeStyles((theme) => ({
@@ -42,6 +43,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const HomePage = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [directions, setDirections] = useState(null);
+
   const classes = useStyles();
 
   return (
@@ -66,7 +70,7 @@ const HomePage = () => {
           </Grid>
           <Grid item xs={12}>
             <Paper className={classes.paper} style={{ height: 500 }}>
-              <MapWrapper markersList={markersList} />
+              <MapWrapper markersList={markersList} directions />
             </Paper>
           </Grid>
         </Grid>
