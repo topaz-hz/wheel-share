3c93a7aa2ab75c2581d6b837e0feea329866e6f5
topaz-hz <topazhersh@gmail.com>
2022-04-20 20:10:07 +0300

    directions api (wip)

--- src/Components/MapAndNavigation/MapWrapper.js
+++ src/Components/MapAndNavigation/MapWrapper.js
@@ -1,10 +1,39 @@
 import React, { useState } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+// import { DirectionsRenderer } from 'react-google-maps';
 import PropTypes from 'prop-types';
+// const google = window.google;
 
-// eslint-disable-next-line react/prop-types
 const MapWrapper = ({ markersList }) => {
+  // const DirectionsService = new google.maps.DirectionsService();
+  // let [directions, setDirections] = useState('');
   const [activeMarker, setActiveMarker] = useState(markersList[0] || null);
+  // const origin = { lat: 6.5244, lng: 3.3792 };
+  // const destination = { lat: 6.4667, lng: 3.45 };
+  //
+  // DirectionsService.route(
+  //   {
+  //     origin: origin,
+  //     destination: destination,
+  //     travelMode: google.maps.TravelMode.WALKING,
+  //     waypoints: [
+  // {
+  //   location: new google.maps.LatLng(6.4698, 3.5852)
+  // },
+  // {
+  //   location: new google.maps.LatLng(6.6018, 3.3515)
+  // }
+  //     ]
+  //   },
+  //   (result, status) => {
+  //     if (status === google.maps.DirectionsStatus.OK) {
+  //       console.log(result);
+  //       setDirections(result);
+  //     } else {
+  //       console.error(`error fetching directions ${result}`);
+  //     }
+  //   }
+  // );
 
   const onMarkerClick = (marker) => {
     setActiveMarker(marker);
@@ -31,6 +60,7 @@ const MapWrapper = ({ markersList }) => {
           // icon={markerTypes[marker.type]}
         />
       ))}
+      {/*{directions && <DirectionsRenderer directions={directions} />}*/}
     </Map>
   );
 };
